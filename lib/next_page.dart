c045156c43625b84f05909d4a3646b7ebd2c6b2b
sen2nd <s1911536@s.tsukuba.ac.jp>
2021-07-27 21:00:20 +0900

    first commit

---
+++ lib/next_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NextPage extends StatelessWidget {
+  // ここにイニシャライザを書く
+  NextPage(this.name);
+  String name;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('KBOYのFlutter大学'),
+      ),
+      body: Container(
+        color: Colors.red,
+        child: Text(name),
+      ),
+    );
+  }
+}
